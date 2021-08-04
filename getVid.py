715c0a625bbd9634f754c8d16b58965c8d77a938
Jang Software <jangwsoftware@gmail.com>
2021-08-03 21:38:10 -0400

    Added GUI

--- getVid.py
+++ getVid.py
@@ -1,37 +1,161 @@
 from selenium.webdriver.firefox.options import Options
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+import selenium
 import random
+import sys
+import threading
+from PySide2.QtWidgets import *
+from PySide2.QtGui import *
+from PySide2.QtCore import *
+import time
 
-def getVideos(numRolls):
-    with open('words.txt', 'r', encoding='utf-8') as w:
-        words = w.readlines()
+class RunRYL(QRunnable):
+    def run(self):
+        mainWindow.getVideos()
 
-    fire_options = Options()
-    fire_options.add_argument('--headless')
-    videosOfDay = []
-    videoBrowser = webdriver.Firefox(options=fire_options)
+class SignalsRYL(QObject):
+    linkSignal = Signal(object)
+    statusSignal = Signal(object)
+
+class window(QWidget):
+    def __init__(self):
+        super().__init__()
+        with open('words.txt', 'r', encoding='utf-8') as w:
+            self.words = w.readlines()
+
+        browserThread = threading.Thread(target=self.setupBrowser)
+        browserThread.start()
+
+        self.threadpool = QThreadPool()
+        
+        self.showTitles = QRadioButton('Show Titles')
+        self.showTitles.setChecked(True)
+        self.showLinks = QRadioButton('Show Links')
+        self.showWords = QRadioButton('Show Words')
+        displayL = QVBoxLayout()
+        displayL.addWidget(self.showTitles)
+        displayL.addWidget(self.showLinks)
+        displayL.addWidget(self.showWords)
+        displayG = QGroupBox('Display')
+        displayG.setLayout(displayL)
+
+        self.addWord = QLineEdit()
+        self.addWord.setPlaceholderText('Add word(s) to search')
+        self.prepend = QRadioButton('Prepend')
+        self.prepend.setChecked(True)
+        self.append = QRadioButton('Append')
+        bonusL = QVBoxLayout()
+        bonusL.addWidget(self.addWord)
+        bonusL.addWidget(self.prepend)
+        bonusL.addWidget(self.append)
+        self.bonusG = QGroupBox('Bonus Word')
+        self.bonusG.setCheckable(True)
+        self.bonusG.setLayout(bonusL)
+
+        self.linkList = QTextBrowser()
+        self.linkList.setOpenExternalLinks(True)
+        self.linkList.setLineWrapMode(QTextEdit.NoWrap)
+        self.signals = SignalsRYL()
+        self.signals.linkSignal.connect(lambda s: self.linkList.append(s))
+        self.signals.statusSignal.connect(lambda s: self.status.setText(s))
+
+        self.numVideos = QSpinBox()
+        start = QPushButton('Run')
+        start.clicked.connect(self.startRYL)
+        start.clicked.connect(lambda: self.linkList.clear())
+        self.status = QLabel()
+        startL = QVBoxLayout()
+        startL.addWidget(self.numVideos)
+        startL.addWidget(start)
+        startL.addWidget(self.status)
+
+
+        topL = QHBoxLayout()
+        topL.addWidget(self.bonusG)
+        topL.addWidget(displayG)
+        topL.addLayout(startL)
+
+        
+
+        mainL = QGridLayout()
+        mainL.addLayout(topL, 0, 0)
+        mainL.addWidget(self.linkList, 1, 0)
+
+        self.setLayout(mainL)
+        self.show()
+
+    def setupBrowser(self):
+        fire_options = Options()
+        fire_options.add_argument('--headless')
+        fp = webdriver.FirefoxProfile()
+        fp.set_preference('media.volume_scale', '0.0')
+        self.videoBrowser = webdriver.Firefox(options=fire_options, firefox_profile=fp)
+
+    def getVideos(self):
+
+        self.signals.statusSignal.emit('Starting up...')
+
+        while True:
+            try:
+                self.videoBrowser
+            except AttributeError:
+                continue
+            break
+            
+        self.signals.statusSignal.emit('Collecting...')
+
+        x = 0
+        while x < self.numVideos.value():
+            self.wordRoll = random.choice(self.words).strip()
+
+            try:
+                self.videoBrowser.get(self.printUrl())
+                video = self.videoBrowser.find_elements_by_id('video-title')
+                r = random.choice(range(1, len(video) - 1))
+                self.videoTitle = video[r].text
+                video[r].click()            
+                x += 1 
+            except IndexError:
+                continue
+            except selenium.common.exceptions.ElementNotInteractableException:
+                continue
+            except selenium.common.exceptions.NoSuchElementException:
+                continue
+
+            self.signals.linkSignal.emit(f'{x}: {self.printLink()}')
+
+        self.signals.statusSignal.emit('Finished!')
+
+    def closeEvent(self, event):
+        self.videoBrowser.close()
+
+    def startRYL(self):
+ 
+        self.ryl = RunRYL()
+        self.threadpool.globalInstance().start(self.ryl)
+
+    def printUrl(self):
+        if not self.bonusG.isChecked():
+            urlGet = f'https://youtube.com/results?search_query={self.wordRoll}+before%3A2012-08-08'
+        else:
+            if self.prepend.isChecked():
+                urlGet = f'https://youtube.com/results?search_query={self.addWord.text()}+{self.wordRoll}+before%3A2012-08-08'
+            elif self.append.isChecked():
+                urlGet = f'https://youtube.com/results?search_query={self.wordRoll}+{self.addWord.text()}+before%3A2012-08-08'
+        return urlGet
+
+    def printLink(self):
+        if self.showTitles.isChecked():
+            link = f'<a href="{self.videoBrowser.current_url}">{self.videoTitle}</a>'
+        elif self.showLinks.isChecked():
+            link = f'<a href="{self.videoBrowser.current_url}">{self.videoBrowser.current_url}</a>'
+        elif self.showWords.isChecked():
+            link = f'<a href="{self.videoBrowser.current_url}">{self.wordRoll}</a>'
+        return link
+
+if __name__ == '__main__':
+    app = QApplication([])
+
+    mainWindow = window()
     
-    x = 0
-    while x < numRolls:
-        wordRoll = random.choice(words)
-        wordRoll2 = random.choice(words)
-
-        try:
-            videoBrowser.get(f'https://youtube.com/results?search_query={wordRoll}+{wordRoll2}+before%3A2012-08-08&safeSearch=moderate') 
-            video = videoBrowser.find_elements_by_id('video-title')
-            r = random.choice(range(1, len(video) - 1))
-            videoTitle = video[r].text
-            video[r].click()
-            videosOfDay.append(f'{videoTitle} : {videoBrowser.current_url}')
-            x += 1 
-        except IndexError:
-            continue
-        except selenium.common.exceptions.ElementNotInteractableException:
-            continue
-        except selenium.common.exceptions.NoSuchElementException:
-            continue
-    videoBrowser.close()
-    return videosOfDay
-
-print(getVideos(5))
+    sys.exit(app.exec_())
